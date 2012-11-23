a096c66d8b19eb9c19b8589a7454a33938ac69ff
Rarst <contact@rarst.net>
2012-11-23 19:32:33 +0200

    Added PHPDoc for handler class.

--- class-r-hook-handler.php
+++ class-r-hook-handler.php
@@ -1,5 +1,8 @@
 <?php
 
+/**
+ * Handler class for events in hooks.
+ */
 class R_Hook_Handler {
 
 	private $data;
@@ -9,27 +12,49 @@ class R_Hook_Handler {
 	 */
 	private $callback;
 
+	/**
+	 * @param mixed         $data
+	 * @param null|callback $callback
+	 */
 	function __construct( $data, $callback = null ) {
 
 		$this->data     = $data;
 		$this->callback = $callback;
 	}
 
+	/**
+	 * Executes callback with custom arguments, ignores what hook passes.
+	 *
+	 * @return mixed|null
+	 */
 	function action() {
 
 		call_user_func_array( $this->callback, $this->data );
 
+		// compatibility with filters
 		if ( func_num_args() )
 			return func_get_arg( 0 );
 
 		return null;
 	}
 
+	/**
+	 * Returns custom data to filter.
+	 *
+	 * @return mixed
+	 */
 	function filter() {
 
 		return $this->data;
 	}
 
+	/**
+	 * Searches and replaces data as array or substring, depending on passed filter value.
+	 *
+	 * @param mixed $input
+	 *
+	 * @return array|mixed
+	 */
 	function replace( $input ) {
 
 		if ( is_array( $input ) )
@@ -40,6 +65,13 @@ class R_Hook_Handler {
 		return $input;
 	}
 
+	/**
+	 * Prepends data to array or string, passed by filter.
+	 *
+	 * @param mixed $input
+	 *
+	 * @return array|string
+	 */
 	function prepend( $input ) {
 
 		if ( is_array( $input ) ) {
@@ -64,6 +96,14 @@ class R_Hook_Handler {
 		return $input;
 	}
 
+
+	/**
+	 * Appends data to array or string, passed by filter.
+	 *
+	 * @param mixed $input
+	 *
+	 * @return array|string
+	 */
 	function append( $input ) {
 
 		if ( is_array( $input ) ) {
